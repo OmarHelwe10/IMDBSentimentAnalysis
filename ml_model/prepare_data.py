c07632b301c2b1c44eff6d58abb8602f6417fd2c
Omar_Helwe <ohelwe@gmail.com>
2025-04-05 09:12:23 +0300

    data_v2

--- ml_model/prepare_data.py
+++ ml_model/prepare_data.py
@@ -5,7 +5,7 @@ df = pd.read_csv("../dataset/IMDB Dataset.csv")
 
 # Convert sentiment labels to binary (0: Negative, 1: Positive)
 df["sentiment"] = df["sentiment"].map({"positive": 1, "negative": 0})
-sample_size=2500
+sample_size=5000
 # Select positive and negative samples
 df_pos = df[df["sentiment"] == 1].sample(sample_size, random_state=42)
 df_neg = df[df["sentiment"] == 0].sample(sample_size, random_state=42)
@@ -15,6 +15,6 @@ df_sample = pd.concat([df_pos, df_neg]).sample(frac=1, random_state=42)
 
 # Save version 1 dataset
 
-df_sample.to_csv("../data_versions/imdb_v1.csv", index=False)
+df_sample.to_csv("../data_versions/imdb_v2.csv", index=False)
 
 print(df_sample["sentiment"].value_counts())  # Ensure class balance
